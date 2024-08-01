efa4d32d58b225d706450b2fc6a40233ebe95d6d
aideity <luchesimartina@gmail.com>
2024-08-01 11:06:44 -0300

    Add files via upload

---
+++ micro1.js
@@ -0,0 +1,21 @@
+let electrodomesticos = ["lavadora", "microondas","notebook", "netbook", "batidor", "heladera"];
+
+console.log(electrodomesticos.length);
+console.log(electrodomesticos[2]);
+let sacarElectrodomesticos = electrodomesticos.shift();
+let ponerElectrodomesticos = electrodomesticos.push("lavadora");
+let newElectrodomesticos = electrodomesticos.unshift("plancha para el pelo", "vaporera");
+console.log(electrodomesticos.length);
+
+// ** Buscar un elemento del array y crear una condición responsable de establecer si existe o no el producto dentro del array. De existir, se le debe mostrar el mensaje:
+//“Producto encontrado”. En el caso contrario mostrar el mensaje “El producto buscado no existe”.
+let unirElectrodomesticos = electrodomesticos.join(" ");
+console.log(unirElectrodomesticos);
+console.log(unirElectrodomesticos.length);
+
+//let replaceElectrodomesticos = electrodomesticos.replace('plancha para el pelo', '0PC'); // tira error, revisar
+
+console.log(electrodomesticos);
+
+let separarCadena = unirElectrodomesticos.split(" ");
+console.log(separarCadena);
