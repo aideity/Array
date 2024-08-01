bb459df9908cba29e8b4a099c573fc30e1976e8b
aideity <luchesimartina@gmail.com>
2024-08-01 11:57:14 -0300

    Add files via upload

---
+++ micro2.js
@@ -0,0 +1,14 @@
+let movies =  ['Turno de día', '30 noches con mi ex', 'Bestia', 'El monte', 'Top gun maverick','Elvis','Thor: amor y trueno'];
+// Más tarde desde la producción indicaron que la película más vendida era: ‘Thor:
+// amor y trueno’. Por lo tanto, en la estructura creada querían ver primero dicha
+// película y con letras en mayúsculas. Para ello el tech leader, te solicita que crees
+// una función, la cual recibirá por parámetro la película indicada y deberá retornar la
+// misma pero en mayúscula. Para ello sería bueno que investigues sobre el método
+// .toUpperCase(). Con ese valor recibido, deberás colocarla primera en la estructura creada.
+
+function mayus(ThorAmoryTrueno) {
+    return 
+}
+
+let newMovies = ['Counter-Strike', 'NOP', 'Vértigo', 'Nick', 'Avatar'];
+let errorMovies = newMovies.shift()
